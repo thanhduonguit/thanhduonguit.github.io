049afe3394b47369d21616597c8db4e482783846
Duong Tan Thanh <thanhduong12a2@gmail.com>
2019-09-26 17:34:46 +0700

    desigin pattern

--- Fresher/responsive/responsive-01/js/main.js
+++ Fresher/responsive/responsive-01/js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
     $("#play-video").click(function() {
         if ($(window).innerWidth() <= 578) {
-            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '300', iframe: true });
+            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '330', iframe: true });
         } else if ($(window).innerWidth() <= 768) {
-            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '400', iframe: true });
+            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '430', iframe: true });
         } else if ($(window).innerWidth() <= 992) {
-            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '450', iframe: true });
+            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '470', iframe: true });
+        } else if ($(window).innerWidth() <= 1300) {
+            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '600', iframe: true });
+        } else if ($(window).innerWidth() <= 1500) {
+            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '700', iframe: true });
         } else {
             $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '95%', iframe: true });
         } 
