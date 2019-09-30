29b98f795191a016a5bbedc8dfc6da5201664847
Duong Tan Thanh <thanhduong12a2@gmail.com>
2019-09-30 17:36:01 +0700

    responsive

--- Fresher/responsive/responsive-02/js/main.js
+++ Fresher/responsive/responsive-02/js/main.js
@@ -1,13 +1,54 @@
-$(document).ready(function() {
-    $("#play-video").click(function() {
-        if ($(window).innerWidth() <= 578) {
-            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '300', iframe: true });
-        } else if ($(window).innerWidth() <= 768) {
-            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '400', iframe: true });
-        } else if ($(window).innerWidth() <= 992) {
-            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '450', iframe: true });
-        } else {
-            $.colorbox({ href: 'https://www.youtube.com/embed/LzwGi3G7-4Y', width: '95%', height: '95%', iframe: true });
-        } 
-    })
-})
+let buttons = $('.btn-text');
+let contents = $('.place-container-images');
+let buttonMore = $('.btn-more');
+let showAll = $('#all');
+let showHokkaido = $('#hokkaido');
+let showTohoku = $('#tohoku');
+let showKanto = $('#kanto');
+let showTokai = $('#tokai');
+let showWest = $('#west');
+
+showAll.click(function() {
+    show('all');
+    $(buttonMore[0]).removeClass('hide');
+});
+
+showHokkaido.click(function() {
+    show('hokkaido');
+    hideButtonMore();
+});
+
+showTohoku.click(function() {
+    show('tohoku');
+    hideButtonMore();
+});
+
+showKanto.click(function() {
+    show('kanto');
+    hideButtonMore();
+});
+
+showTokai.click(function() {
+    show('tokai');
+    hideButtonMore();
+});
+
+showWest.click(function() {
+    show('west');
+    hideButtonMore();
+});
+
+function show(id) {
+    for (let i = 0; i < buttons.length; i++) {
+        $(buttons[i]).removeClass('btn-text-active');
+        $(contents[i]).removeClass('active');
+    }
+    $('#' + id).addClass('btn-text-active');
+    $('#place_' + id).addClass('active');
+}
+
+function hideButtonMore() {
+    buttonMore[0].className += ' hide';
+}
+
+show('all');
