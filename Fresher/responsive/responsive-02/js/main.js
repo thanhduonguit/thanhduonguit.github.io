63e3d5ecef505b8836a1c594ad98748785166d57
Duong Tan Thanh <thanhduong12a2@gmail.com>
2019-10-01 14:29:14 +0700

    responsive

--- Fresher/responsive/responsive-02/js/main.js
+++ Fresher/responsive/responsive-02/js/main.js
@@ -1,42 +1,15 @@
-let buttons = $('.btn-text');
-let contents = $('.place-container-images');
-let buttonMore = $('.btn-more');
-let showAll = $('#all');
-let showHokkaido = $('#hokkaido');
-let showTohoku = $('#tohoku');
-let showKanto = $('#kanto');
-let showTokai = $('#tokai');
-let showWest = $('#west');
+let buttons = $('.js-btn-text');
+let contents = $('.js-place-container-images');
+let buttonMore = $('.js-btn-more');
 
-showAll.click(function() {
-    show('all');
-    $(buttonMore[0]).removeClass('hide');
-});
-
-showHokkaido.click(function() {
-    show('hokkaido');
-    hideButtonMore();
-});
-
-showTohoku.click(function() {
-    show('tohoku');
-    hideButtonMore();
-});
-
-showKanto.click(function() {
-    show('kanto');
-    hideButtonMore();
-});
-
-showTokai.click(function() {
-    show('tokai');
-    hideButtonMore();
-});
-
-showWest.click(function() {
-    show('west');
-    hideButtonMore();
-});
+buttons.click(function() {
+    show(($(this).attr("id")));
+    if($(this).attr("id") == "all") {
+        $(buttonMore[0]).removeClass('hide');
+    }else {
+        buttonMore[0].className += ' hide';
+    }
+})
 
 function show(id) {
     for (let i = 0; i < buttons.length; i++) {
@@ -45,10 +18,6 @@ function show(id) {
     }
     $('#' + id).addClass('btn-text-active');
     $('#place_' + id).addClass('active');
-}
-
-function hideButtonMore() {
-    buttonMore[0].className += ' hide';
 }
 
 show('all');
