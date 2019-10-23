cf654baa42435b71e7407ce0df03b75c286393b2
Duong Tan Thanh <thanhduong12a2@gmail.com>
2019-10-23 08:42:36 +0700

    scss 02

---
+++ Fresher/SCSS-Exercise/scss_02/js/main2.js
@@ -0,0 +1,10 @@
+$(".js__section__title").click(function() {
+    if (!$(this).hasClass("active")) {
+        $(this).addClass("active").siblings().removeClass("active");
+        $(this).next().css("display", "none")
+    } else {
+        $(this).removeClass("active");
+        $(this).next().css("display", "flex")
+    }
+    (this).classList.toggle("change");
+});
