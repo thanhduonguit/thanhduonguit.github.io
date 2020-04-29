344503d05bb54e74c633f95dde60711c79c94c52
Minh Anh Nguyen <minhna@runsystem.net>
2020-04-29 09:11:08 +0700

    test

--- Fresher/table-scroll/js/main.js
+++ Fresher/table-scroll/js/main.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
 
-    // Handle scroll vertical of data table
-    $('#example, #example2, #example3').DataTable( {
-        "scrollY":        "600px",
-        "scrollCollapse": true,
-        "paging":         false
-    });
-
     // Handle click active menu
     $('ul.js-nav li').click(function(){
 		$('ul.js-nav li').removeClass('active');
@@ -14,19 +7,49 @@ $(document).ready(function() {
     });
 
     // Handle click scroll to table content
-    $('.rika-tab').click(function(){
+    $('.section-01').click(function(){
         $('html, body').animate({
-            scrollTop: $("#rika-tab").offset().top - 80
+            scrollTop: $("#table-01").offset().top - 132
         }, 600);
     });
-    $('.syakai-tab').click(function(){
+    $('.section-02').click(function(){
         $('html, body').animate({
-            scrollTop: $("#syakai-tab").offset().top - 80
+            scrollTop: $("#table-02").offset().top - 132
         }, 600);
     });
-    $('.tab-03').click(function(){
+    $('.section-03').click(function(){
         $('html, body').animate({
-            scrollTop: $("#tab-03").offset().top - 80
+            scrollTop: $("#table-03").offset().top - 132
         }, 600);
+    });
+
+    // Handle croll fixed table
+    $(window).scroll(function(){
+        var height01 = $("#section-01").height();
+        var height02 = $("#section-02").height();
+        var timer = false;
+        var thisPosition = 0;
+        
+        if (timer !== false) {
+            clearTimeout(timer);
+        }
+
+        timer = setTimeout(function() {
+            thisPosition = $(window).scrollTop();
+            if (thisPosition < height01) {
+                $("#section-01").addClass('active');
+                $("#section-02").removeClass('active');
+                $("#section-03").removeClass('active');
+            } else if (height01 <= thisPosition < (height01 + height02)) {
+                $("#section-02").addClass('active');
+                $("#section-01").removeClass('active');
+                $("#section-03").removeClass('active');
+            } 
+            if (thisPosition >= (height01 + height02)) {
+                $("#section-03").addClass('active');
+                $("#section-01").removeClass('active');
+                $("#section-02").removeClass('active');
+            }
+        });
     });
 });
